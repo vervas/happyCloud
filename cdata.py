ee62ab05610851b7de9d86dca692d6ed9fe073c7
Dimitris Verraros <vervas.g@gmail.com>
2013-09-02 14:00:00 +0200

    wip

--- cdata.py
+++ cdata.py
@@ -6,8 +6,15 @@ class CData:
     def __init__(self):
         self.api = API()
 
-    def create_token(email,password):
-        self.api.create_token(email,password)
+    def create_token(self, email, password):
+        self.api.create_token(email, password)
 
-    def get_apps():
-        return api.read_apps()
+    def get_apps(self):
+        return [i['name'] for i in self.api.read_apps()]
+
+    def has_default_dep(self, appname):
+        try:
+            return self.api.read_deployment(appname, 'default')
+        except:
+            return False
+
